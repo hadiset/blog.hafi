a161b5657a00d410bbd09b14a27543db3661f731
hadiset <hadynwaites@gmail.com>
2025-07-07 09:47:52 +0700

    fixing logic for close menu button

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,5 @@
-document.getElementById('mobile-menu-button').addEventListener('click', function() {
-  const mobileMenu = document.getElementById('mobile-menu');
-  if (mobileMenu) {
-    mobileMenu.classList.toggle('hidden');
-  }
+document.getElementById('mobile-menu-button')?.addEventListener('click', () => {
+  document.getElementById('mobile-menu')?.classList.toggle('hidden');  
+  document.getElementById('open-menu-button')?.classList.toggle('hidden');  
+  document.getElementById('close-menu-button')?.classList.toggle('hidden');  
 });
