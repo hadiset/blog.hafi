2cc0e7d488e7b503ca13d5812e17eb2c1866677b
hadiset <hadynwaites@gmail.com>
2025-10-16 13:15:59 +0700

    Update Blog Name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.hafi.click', 
+  site: 'https://hafi.click', 
   base: '/',
   integrations: [    
     mdx(),
