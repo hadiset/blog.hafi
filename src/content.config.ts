22c4ffae74c6d29231d427d5185284453c5d0d4b
Root (Linus Torvalds) <root@evonic.local>
2026-07-01 11:15:53 +0700

    upgrade astro version

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
-const blogCollection = defineCollection({
-  type: 'content',
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
@@ -17,6 +19,4 @@ const blogCollection = defineCollection({
   })
 });
 
-export const collections = {
-  'blog': blogCollection
-}
+export const collections = { blog };
