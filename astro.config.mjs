b313bfe3eea65058530125c3f6f8e5576da54c9c
Root (Linus Torvalds) <root@evonic.local>
2026-07-01 09:28:22 +0700

    change domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hafi.click', 
+  site: 'https://hadiset.com', 
   base: '/',
   integrations: [    
     mdx(),
